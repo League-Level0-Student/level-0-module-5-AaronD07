149ccd9737d5783351658c9fd11b2349721a3db2
AaronD07 <38016960+AaronD07@users.noreply.github.com>
2019-07-03 22:28:35 -0700

    :smile:

--- src/_06_for_loop_gauntlet/forLoopGauntlet.java
+++ src/_06_for_loop_gauntlet/forLoopGauntlet.java
@@ -3,37 +3,37 @@ import javax.swing.JOptionPane;
 
 public class forLoopGauntlet {
 	public static void main(String[] args) {
-//		for(int a=0; a<101; a++) {
-//			JOptionPane.showMessageDialog(null, a);
-//		}
-//		for(int b=0; b>-1; b--) {
-//			
-//			
-//		JOptionPane.showMessageDialog(null, b);
-//	}	
-//		
-//		
-//		for(int c=1; c<101; c++) {
-//			
-//			
-//			JOptionPane.showMessageDialog(null, c*2);
-//
-//}
-//		for(int e=0; e<501; e++) {
-//			if(e%2==0) {
-//				JOptionPane.showMessageDialog(null, e + " even");
-//			}
-//			else {
-//				JOptionPane.showMessageDialog(null, e +" odd");
-//			}
-//	}
-//			for(int f=0; f<778; f++) {
-//				if(f%7==0) {
-//					JOptionPane.showMessageDialog(null, f);
-//				}
-//				
-//		}
-//			
+		for(int a=0; a<101; a++) {
+			JOptionPane.showMessageDialog(null, a);
+		}
+		for(int b=0; b>-1; b--) {
+			
+			
+		JOptionPane.showMessageDialog(null, b);
+	}	
+		
+		
+		for(int c=1; c<101; c++) {
+			
+			
+			JOptionPane.showMessageDialog(null, c*2);
+
+}
+		for(int e=0; e<501; e++) {
+			if(e%2==0) {
+				JOptionPane.showMessageDialog(null, e + " even");
+			}
+			else {
+				JOptionPane.showMessageDialog(null, e +" odd");
+			}
+	}
+			for(int f=0; f<778; f++) {
+				if(f%7==0) {
+					JOptionPane.showMessageDialog(null, f);
+				}
+			
+		}
+			
 		
 			for(int i=1; i<101; i++) {
 				if(i%10==0) {
@@ -54,22 +54,22 @@ public class forLoopGauntlet {
 			
 			}
 			
-//			for(int i = 1; i<7; i++) {
-//				for(int j = 0; j<i; j++) {
-//					
-//				
-//				System.out.print("* ");
-//			}
-//				System.out.println("");
-//			}
-//			
-//			
-//			
-//			for(int b=0; b>-1; b--) {
-//				
-//				
-//				JOptionPane.showMessageDialog(null, b);
-//			}	
+			for(int i = 1; i<7; i++) {
+				for(int j = 0; j<i; j++) {
+					
+				
+				System.out.print("* ");
+			}
+				System.out.println("");
+			}
+			
+			
+			
+			for(int b=0; b>-1; b--) {
+				
+				
+				JOptionPane.showMessageDialog(null, b);
+			}	
 				
 			
 			
